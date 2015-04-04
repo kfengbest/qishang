11e8c79305bbeda5c969c40bff17fb9dfcc3e310
Guogen Liang <guogen.liang@outlool.com>
2015-04-04 18:29:04 +0800

    update order status rule

--- qishang/public/modules/orders/filters/orderStatusFilter.js
+++ qishang/public/modules/orders/filters/orderStatusFilter.js
@@ -3,8 +3,8 @@
 angular.module('orders').filter('orderStatus', function () {
 	return function(status) {
 		var message;
-	    // -1：已取消;  -2: 订单过期; -10：配送失败
-		//  0: 待付款； 1： 待确认； 2： 配送中； 3： 配送完成； 10： 订单关闭
+		// -1：已取消;  -2: 订单过期; -10：配送失败
+		//  0: 待付款； 1： 待确认； 2: 支付已确认； 5： 配送中； 6： 配送完成； 10： 订单关闭
 		switch(status) {
 			case -1:
 				message = '已取消';
@@ -12,7 +12,7 @@ angular.module('orders').filter('orderStatus', function () {
 			case -2:
 				message = '订单过期';
 				break;
-			case -3:
+			case -10:
 				message = '配送失败';
 				break;
 			case 0:
@@ -22,12 +22,15 @@ angular.module('orders').filter('orderStatus', function () {
 				message = '待确认';
 				break;
 			case 2:
+				message = '支付已确认';
+				break;
+			case 5:
 				message = '配送中';
 				break;
-			case 3:
+			case 6:
 				message = '配送完成';
 				break;
-			case 4:
+			case 10:
 				message = '订单关闭';
 				break;
 			default:
